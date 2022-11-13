045a1d30aa0552c097c2d7a2a7cad9ed9a4147c6
eunjiiiiii <ej27470235@gmail.com>
2022-11-13 18:30:56 +0900

    <feat>: add saving img

--- backend/application.py
+++ backend/application.py
@@ -25,32 +25,34 @@ def gen():
     """Video streaming generator function."""
     #if request.method == 'POST':
     t0 = time.time()
+    i = 0
     while True:
         success, frame = vc.read()
 
         if time.time() - t0 > 0.8:
-            encode_return_code, image_buffer = cv2.imencode('.jpg', frame)
-            io_buf = io.BytesIO(image_buffer)
-
-            results = model(frame, size=320)  # reduce size=320 for faster inference
-            # print(results)
-            objs = results.pandas().xyxy[0]['name']
-            result = results.pandas().xyxy[0].to_json(orient = 'records')
-
-            print(objs)
-            print(result)
-            #objs.render()  # 결과 렌더링
-
-            # 부정행위 감지
-            cheating_list = detect_cheating(objs)
-            print(cheating_list)
-            # 부정행위 경고 메세지 출력
-            warning_msg = gen_warning_msg(cheating_list)
-            print(warning_msg)
+            #encode_return_code, image_buffer = cv2.imencode('.jpg', frame)
+            #io_buf = io.BytesIO(image_buffer)
 
             if not success:
                 break
             else:
+                results = model(frame, size=320)  # reduce size=320 for faster inference
+                # print(results)
+                objs = results.pandas().xyxy[0]['name']
+                json_result = results.pandas().xyxy[0].to_json(orient='records')
+                result = results.pandas().xyxy[0]
+
+                print(result['name'])
+                #print(result)
+                # objs.render()  # 결과 렌더링
+
+                # 부정행위 감지
+                cheating_list = detect_cheating(result['name'])
+                print(cheating_list)
+                # 부정행위 경고 메세지 출력
+                warning_msg = gen_warning_msg(cheating_list)
+                print(warning_msg)
+
                 ret, buffer = cv2.imencode('.jpg', results.ims[-1])  # 더 효율적인 방법이 있을까?
                 frame = buffer.tobytes()  # 바이트로 변환
 
@@ -60,6 +62,9 @@ def gen():
 
                 t0 = time.time()  # 새로운 기준 시간 측정
 
+                if len(cheating_list) > 0:
+                    # 부정행위가 감지되면
+                    cv2.imwrite('cheating_img.png',frame) # 이미지 파일 저장
                 yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
         else:
             # 1초 안넘었을 경우 모델에 넣지 않고 프레임만 보냄
@@ -67,7 +72,10 @@ def gen():
                 _, buffer = cv2.imencode('.jpg', frame)  # 더 효율적인 방법이 있을까?
                 frame = buffer.tobytes()  # 바이트로 변환
                 yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        i+=1
 
+    vc.release()
+    cv2.destroyAllWindows()
 @app.route('/video_feed')
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
@@ -86,10 +94,14 @@ def detect_cheating(result):
     Returns: 부정행위에 포함되는 객체 list
 
     """
-    cheating_obj_list = ['cell phone', 'person', 'book']
+    cheating_obj_list = ['cell phone', 'book']
     cheating_list = []
+    person_cnt = 0
     for i, obj in enumerate(result):
-        if obj in cheating_obj_list:
+        if obj == 'person':
+            # 사람 수 2명 이상이면 부정행위로 인식
+            person_cnt += 1
+        if (obj in cheating_obj_list) or (obj == 'person' and person_cnt > 1):
             cheating_list.append(obj)
 
     return cheating_list
